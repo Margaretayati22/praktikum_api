8beca6379be55070d672d2c310f771abfa5381d2
gawoh <gtmiii23@gmail.com>
2025-07-30 13:59:14 +0800

    transaction

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.use(express.json());
 app.get('/', (req, res) => res.send('API aktif!'));
 
 // tambahkan routing kamu dibawah sini
+const transactionRoutes = require('./routes/transaction.route');
+
+app.use('/api/transactions', transactionRoutes);
 
 
 // Koneksi ke database
